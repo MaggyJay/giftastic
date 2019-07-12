9803ba31168d5d76be30b344592f6973e2c5e737
Magdalene <magdalene809@gmail.com>
2019-07-12 03:36:00 -0500

    Got the code to start working and verified it was communicating with giphy api

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -1,58 +1,79 @@
 var personList = [];
 $(document).ready(function () {
 
-    function displayGifs () {
+    $('#submit-button').on('click', function (event) {
+        event.preventDefault();
+        //function displayGifs() {
         //we have to use this so it can apply this to whatever person button exists
-        var queryURL = "https://api.giphy.com/v1/gifs/searchq=" + person + "&api_key=PYBhZj8kiUoPbeAbyY26QynXWxzncbRX"
-        var person= $(this).attr("data-person");
+
+        //console.log('Nicki is being clicked');
+
+        var userInput = $('#person').val().trim();
+
+        //var person = $(this).attr("data-person");
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + userInput + "&api_key=PYBhZj8kiUoPbeAbyY26QynXWxzncbRX&limit=10";
+
 
         $.ajax({
-            url:queryURL,
-            method:"GET"
+            url: queryURL,
+            method: "GET"
 
-        }).then(function(response) {
-
-            console.log(response);
-            var results = response.data;
-
-            for (var i=0; i < results.length; i++){
-                var gifDiv= $('<div>');
-
-                var rating= results[i].rating;
-                var a = $('<p>').text('Rating: ' + rating);
-            }
-            //$('#fav-people-container').text
         })
-    }
 
-    $('#submit-button').on('click', function (response) {
-        response.preventDefault();
-        //console.log('this does as expected!');
+            .then(function (response) {
+                console.log(queryURL);
+                console.log('Youre searching by' + userInput);
 
-       
-        var userInput = $('#person').val().trim();
-        personList.push(userInput);
+                console.log(response);
+                var results = response.data;
 
-        
-        console.log(personList);
-        renderButtons();
+                for (var i = 0; i < results.length; i++) {
+                    var gifDiv = $('<div>');
 
+                    var rating = results[i].rating;
+                    var a = $('<p>').text('Rating: ' + rating);
 
+                    var personImage = $('<img>');
+
+                    personImage.attr('src', results[i].images.fixed_height.url);
+                    gifDiv.prepend(a);
+                    gifDiv.prepend(personImage);
+
+                    $('#fav-people-container').prepend(gifDiv);
+                }
+                //console.log('Were logging the person' + person);
+            })
+           // console.log(response);
     })
 
-    function renderButtons() {
+    // $('#submit-button').on('click', function (response) {
+    //     response.preventDefault();
+    //     console.log('this does as expected!');
+    //     console.log(response);
 
-        $('fav-people-container').empty();
+    //     var userInput = $('#person').val().trim();
+    //     personList.push(userInput);
 
-        for (var y = 0; y < personList.length; y++) {
-            var p = $('<button>');
-            p.addClass("person");
-            p.attr("data-person", personList[y])
-            p.text(personList[y]);
-            $('#fav-people-container').append(p);
-        }
-       
 
-    }
- 
+    //     console.log(personList);
+    //     renderButtons();
+
+
+    // })
+
+    // function renderButtons() {
+
+    //     $('fav-people-container').empty();
+
+    //     for (var y = 0; y < personList.length; y++) {
+    //         var p = $('<button>');
+    //         p.addClass("person");
+    //         p.attr("data-person", personList[y])
+    //         p.text(personList[y]);
+    //         $('#fav-people-container').append(p);
+    //     }
+
+
+    // }
+
 });
