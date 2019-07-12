e703443c135cd4e3b11e168ed6f2121346563617
Magdalene <magdalene809@gmail.com>
2019-07-12 01:21:31 -0500

    Making some changes to the js

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -1,10 +1,33 @@
 var personList = [];
 $(document).ready(function () {
 
+    function displayGifs () {
+        //we have to use this so it can apply this to whatever person button exists
+        var queryURL = "https://api.giphy.com/v1/gifs/searchq=" + person + "&api_key=PYBhZj8kiUoPbeAbyY26QynXWxzncbRX"
+        var person= $(this).attr("data-person");
+
+        $.ajax({
+            url:queryURL,
+            method:"GET"
+
+        }).then(function(response) {
+
+            console.log(response);
+            var results = response.data;
+
+            for (var i=0; i < results.length; i++){
+                var gifDiv= $('<div>');
+
+                var rating= results[i].rating;
+                var a = $('<p>').text('Rating: ' + rating);
+            }
+            //$('#fav-people-container').text
+        })
+    }
 
     $('#submit-button').on('click', function (response) {
         response.preventDefault();
-        console.log('this does as expected!');
+        //console.log('this does as expected!');
 
        
         var userInput = $('#person').val().trim();
@@ -26,7 +49,7 @@ $(document).ready(function () {
             p.addClass("person");
             p.attr("data-person", personList[y])
             p.text(personList[y]);
-           // $('#fav-people-container').append(p);
+            $('#fav-people-container').append(p);
         }
        
 
