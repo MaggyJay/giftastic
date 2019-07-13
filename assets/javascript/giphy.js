0443a057aeeb7d61210f0857dc9f2d6fd17c6e51
Magdalene <magdalene809@gmail.com>
2019-07-13 17:15:56 -0500

    Made final changes to the javascript made a function to call back to the actual api so button would work

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -1,86 +1,58 @@
 var personList = [];
 $(document).ready(function () {
 
-    $('#submit-button').on('click', function mainStuff (event) {
-        event.preventDefault();
-        //function displayGifs() {
-        //we have to use this so it can apply this to whatever person button exists
-
-        //console.log('Nicki is being clicked');
-
-        var userInput = $('#person').val().trim();
-
-        //var person = $(this).attr("data-person");
+    //passing customized value in as the functions argument(parameter)
+    function callAPI(userInput) {
         var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + userInput + "&api_key=PYBhZj8kiUoPbeAbyY26QynXWxzncbRX&limit=10";
-
 
         $.ajax({
             url: queryURL,
             method: "GET"
+        }).then(function (response) {
+            //console.log(queryURL);
+            //console.log('Youre searching by' + userInput);
 
+            console.log(response);
+            var results = response.data;
+
+            for (var i = 0; i < results.length; i++) {
+                var gifDiv = $('<div>');
+
+                var rating = results[i].rating;
+                var a = $('<p>').text('Rating: ' + rating);
+
+                var personImage = $('<img>');
+
+                personImage.attr('src', results[i].images.fixed_height.url);
+                gifDiv.prepend(a);
+                gifDiv.prepend(personImage);
+
+                $('#fav-people-container').prepend(gifDiv);
+            }
         })
+    }
 
-            .then(function (response) {
-                //console.log(queryURL);
-                //console.log('Youre searching by' + userInput);
+    $('#submit-button').on('click', function mainStuff(event) {
+        event.preventDefault();
 
-                console.log(response);
-                var results = response.data;
+        var userInput = $('#person').val().trim();
+        personList.push(userInput);
 
-                for (var i = 0; i < results.length; i++) {
-                    var gifDiv = $('<div>');
+        callAPI(userInput);
 
-                    var rating = results[i].rating;
-                    var a = $('<p>').text('Rating: ' + rating);
-
-                    var personImage = $('<img>');
-
-                    personImage.attr('src', results[i].images.fixed_height.url);
-                    gifDiv.prepend(a);
-                    gifDiv.prepend(personImage);
-
-                    $('#fav-people-container').prepend(gifDiv);
-                }
-
-                //console.log('this does as expected!');
-                //console.log(response);
-
-               // var userInput = $('#person').val().trim();
-                personList.push(userInput);
-
-
-                //console.log(personList);
-                
-                renderButtons();   //console.log('Were logging the person' + person);
-            })
-
-            
-
-
-        // console.log(response);
+        renderButtons();   //console.log('Were logging the person' + person);
     })
 
-
-    $('.person').on('click', function () {
+    //previously i had this as click event on the person class but this was not working due it loading later in the webpage. what document does is say that anywhere on this html document anything with the person class is clicked to perform this action. 
+    $(document).on('click', '.person', function () {
 
         console.log('Do you at least work?');
-        mainStuff();
+        console.log(this);
+        callAPI(this.textContent);
+
+
 
     })
-    // $('#submit-button').on('click', function (response) {
-    //     response.preventDefault();
-    //     console.log('this does as expected!');
-    //     console.log(response);
-
-    //     var userInput = $('#person').val().trim();
-    //     personList.push(userInput);
-
-
-    //     console.log(personList);
-    //     //renderButtons();
-
-
-    // })
 
     function renderButtons() {
 
@@ -96,6 +68,4 @@ $(document).ready(function () {
 
 
     }
-    
-
 });
